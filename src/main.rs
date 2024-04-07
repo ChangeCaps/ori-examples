3c7d54fc3f0d71fcd8af7290a0b9819b0a3b7bfa
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2024-04-07 22:49:31 +0200

    Update examples

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod launch;
 
 use ori::prelude::*;
+use url::Url;
 
 fn button_example() {
     fn ui(_: &mut ()) -> impl View {
@@ -13,13 +14,37 @@ fn button_example() {
     launch::launch_example((), ui, 400, 300);
 }
 
-fn main() {
+fn invalid_example() {
+    fn ui(_: &mut ()) -> impl View {
+        center(text!("Invalid example"))
+    }
+
+    launch::launch_example((), ui, 400, 300);
+}
+
+fn show_example(example: &str) {
+    match example {
+        "button" => button_example(),
+        _ => invalid_example(),
+    }
+}
+
+fn main() -> anyhow::Result<()> {
     console_error_panic_hook::set_once();
 
     let location = web_sys::window().unwrap().location();
+    let href = location.href().map_err(|e| anyhow::anyhow!("{:?}", e))?;
+    let url = Url::parse(&href)?;
 
-    match location.pathname().unwrap().as_str() {
-        "/button" => button_example(),
-        _ => {}
+    let example = url
+        .query_pairs()
+        .find_map(|(key, value)| (key == "example").then_some(value));
+
+    if let Some(example) = example {
+        show_example(&example);
+    } else {
+        invalid_example();
     }
+
+    Ok(())
 }
